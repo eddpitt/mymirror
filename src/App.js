7d4e3f96b4a0346d5d4bdb6f42753c60a34c689c
Edd Pitt <eddpitt@Edds-MacBook-Pro.local>
2018-10-24 20:51:08 +0100

    split into three columns

--- src/App.js
+++ src/App.js
@@ -2,70 +2,66 @@ import React, { Component } from 'react';
 import {Grid, List, Progress, Divider, Checkbox} from 'semantic-ui-react'
 import './App.css';
 class App extends Component {
-  buildLinks(activityName, activityProgress) {
-    return <List.Item key={activityName}>
+
+  buildProgressBar(activityName, activityProgress) {
+    return ( 
+    <List.Item key={activityName}>
+      <div className="progressBar">
       {activityName}
       <Progress percent={activityProgress} progress/> 
-    </List.Item> 
+      </div>
+    </List.Item> )
   }
+
+  buildToDo(thing) {
+    return (  <div className="Item">
+               <Checkbox label={thing} />
+              </div>)
+  }
+
   render() {
     return (
       <div className="App">
-        <Grid container columns={1} stackable>
-        <Grid.Column>
+        <div className="leftColumn Column">
+          <Grid container columns={1} stackable>
+            <Grid.Column>
+              <Divider />
+              <h1>To do this week</h1>
+              <div className="header">
+                {this.buildToDo('Sniff some socks')}
+              </div>
+              <Divider />
+              <h1>Long-term goals</h1>
+              <div className="header">
+                {this.buildToDo('Write a song')}
+              </div>
+            <Divider />
+            <div className="Random">
+              <label> Random act of kindness <Checkbox toggle/> </label>
+            </div>
+            <Divider />
+            <h1>Challenges this week</h1>
+            <div className="Challenges">
+              <List>
+              {this.buildProgressBar('Exercise',44)}
+              {this.buildProgressBar('Music',20)}
+              </List>
+            </div>
+            </Grid.Column>
+          </Grid>
+          <Divider />
+          <Grid container columns={2} stackable>
+            <Grid.Column>
+              <h1>Naughty things to avoid</h1>
+            </Grid.Column>
+          </Grid>
+        </div>
+        <div className="middleColumn  Column"> 
+        </div>
+        <div className="rightColumn  Column"> 
         <Divider />
-        <h1>To do this week</h1>
-        <div className="ToDo">
-        <div className="Item">
-          <div className="Checkbox">
-            <input type="checkbox" name="Washing" />
-            <label> Washing</label>
-          </div>
         </div>
-        <div className="Item">
-          <div className="Checkbox">
-            <input type="checkbox" name="Pay rent" />
-            <label> Pay rent</label>
-          </div>
-        </div>
-        <div className="Item">
-          <div className="Checkbox">
-            <input type="checkbox" name="Shave" />
-            <label> Shave</label>
-          </div>
-        </div>
-        </div>
-        <Divider />
-        <h1>Long-term goals</h1>
-        <div className="Goals">
-        <div className="Item">
-          <div className="Checkbox">
-            <input type="checkbox" name="Write a song" />
-            <label> Write</label>
-          </div>
-        </div>
-        </div>
-        <Divider />
-        <div className="Random">
-          <label> Random act of kindness <Checkbox toggle/> </label>
-        </div>
-        <Divider />
-        <h1>Challenges this week</h1>
-        <div className="Challenges">
-          <List>
-          {this.buildLinks('Exercise',44)}
-          {this.buildLinks('Music',20)}
-          </List>
-        </div>
-        </Grid.Column>
-        </Grid>
-        <Divider />
-        <Grid container columns={2} stackable>
-        <Grid.Column>
-        <h1>Naughty things to avoid</h1>
-        </Grid.Column>
-        </Grid>
-      </div>
+      </div> 
     );
   }
 }
